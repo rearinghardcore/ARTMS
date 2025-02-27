31691327e0a348f7aac57b47738b99742562e981
Ree <rearinghardcore14@gmail.com>
2025-02-28 02:32:37 +0800

    Fixed Registration Error

--- ARTM_Portal/app/Models/User.php
+++ ARTM_Portal/app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'student_id',
         'password',
         'notification',
         'notification_timestamp',
